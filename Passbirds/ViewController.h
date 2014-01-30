5a6cba3db6ebcfcae388973fd905cd76b2f7041f
Ryan <ryeguy_24@yahoo.com>
2014-01-29 21:54:28 -0500

    Initial

---
+++ Passbirds/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Passbirds
+//
+//  Created by Ryan Hittner on 1/29/14.
+//  Copyright (c) 2014 Ryan Hittner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Passbirds/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Passbirds
+//
+//  Created by Ryan Hittner on 1/29/14.
+//  Copyright (c) 2014 Ryan Hittner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
